57c4dc20fb9d39d27345d6436fafddeb23be990a
santanukumrnayak02@gmail.com <you@example.com>
2026-03-05 18:06:45 +0530

    First version

--- frontend/healthcare-frontend/src/components/FormValidate.jsx
+++ frontend/healthcare-frontend/src/components/FormValidate.jsx
@@ -1,16 +1,20 @@
+import "./reform.css";
 export default function FormValidate({data , error, backClick, nextClick}){
     
 return(
-    <form>
+  <div className="form-div">
+    <form className="form-something">
       {data.map((input) => (
         <div key={input.id}>
           <label>{input.name}</label>
+          <br />
           <input
             type={input.type}
             value={input.value}
             onChange={(e) => input.onChange(e.target.value)}
             placeholder={input.placeholder}
           />
+          <br />
           {error[input.name?.toLowerCase()?.replace("*","")] && (
             <p style={{ color: "red" }}>
             {error[input.name?.toLowerCase()?.replace("*","")]}
@@ -21,5 +25,7 @@ return(
       <button onClick={backClick}>Back</button>
       <button onClick={nextClick}>Next</button>
     </form>
+
+  </div>
 )
 }
